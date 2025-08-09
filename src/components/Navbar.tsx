fe98173724021af7b4f665847bd90348e998e424
andikaryan <andikaryan2002@gmail.com>
2025-08-09 14:03:00 +0700

    feat: remove unused imports from Navbar and page components

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import React, { useState } from 'react';
-import ThemeToggle from './ThemeToggle';
 
 export default function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
