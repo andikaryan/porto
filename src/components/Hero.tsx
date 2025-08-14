473b9a644c37facae86e569afab7e782570d51ee
andikaryan <andikaryan2002@gmail.com>
2025-08-14 11:03:52 +0700

    refactor : link CV

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,7 +5,7 @@ export default function Hero() {
     <section className="min-h-screen flex flex-col justify-center items-center bg-gradient-to-b from-blue-900 to-gray-900 pt-24" id="hero">
       <h1 className="text-4xl md:text-6xl font-bold mb-4 text-white">Hi, I am <span className="text-blue-400">Andika Ryan Nurshodiq</span></h1>
       <p className="text-lg md:text-2xl text-gray-300 mb-8">Web Developer</p>
-      <a href="https://drive.google.com/file/d/1RzuVwiisWmgnvPn4pByF52ivikKN2Z4B/view?usp=sharing" className="px-6 py-3 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition">Lihat Resume</a>
+      <a href="https://drive.google.com/file/d/1Gtvty5kxeKF3fPZ8Hml6v74mfzx54hA7/view?usp=sharing" className="px-6 py-3 bg-blue-600 text-white rounded-lg shadow hover:bg-blue-700 transition">Lihat Resume</a>
     </section>
   );
 }
